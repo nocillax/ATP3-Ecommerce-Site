b13ad700124fec66151fe62e0a4865dfb06df3d7
nocillax_sp8 <asifjarif@gmail.com>
2025-05-25 09:10:03 +0600

    created next frontend

---
+++ ecom-site-frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the E-commerce Site</h1>
+      <p>This is the home page of our e-commerce application.</p>
+      <p>Explore our products and enjoy shopping!</p>
+      <p>Check out the latest deals and offers.</p>
+      <p>Happy Shopping!</p>
+    </div>
+  )
+}
+
+export default Home
