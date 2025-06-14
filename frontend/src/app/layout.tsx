c20f7033e94d30ba7933a8a80960254d41b7164a
nocillax_pc <asifjarif@gmail.com>
2025-06-15 04:39:45 +0600

    frontend started

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Reem_Kufi } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const reemKufi = Reem_Kufi({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-reem-kufi",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={reemKufi.variable}>{children}</body>
     </html>
   );
 }
